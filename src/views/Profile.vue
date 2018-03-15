baa9846f78660b9dd8b684d77e4cc84e0da57188
Maija <hello@maijagrudule.com>
2018-03-15 17:03:52 +0100

    [W8] Profile

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -3,7 +3,22 @@
   <div class="page">
 
    <h1>{{ msg }}</h1>
+   <form class="edit input-group col-md-offset-4 col-md-4" @submit.prevent="updateUser(user)">
+
+              <div class="input input-with-icon full-width ">
+                  <input class="input" type="text" v-model="user.name">
+                  <i class="input-icon fa fa-user"></i>
+              </div>
+              <div class="input input-with-icon full-width ">
+                    <input type="email" v-model="user.email">
+                    <i class="input-icon fa fa-envelope"></i>
+               </div>
+
+                    <button class="button" type="submit">Update</button>
+    </form>
+    <div class="col-md-offset-2 col-md-8">
 <canvas id="planet-chart"></canvas>
+</div>
   </div>
 
 </template>
@@ -16,7 +31,8 @@ export default {
   data () {
     return {
       msg: 'Profile page',
-      planetChartData
+      planetChartData,
+      user: { name: "Maija", email: "me@this.com"}
     }
   },
   mounted() {
