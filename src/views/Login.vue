53cc9070bbc8a836165d13b4f01dac3d599dc651
Maija <hello@maijagrudule.com>
2018-03-15 17:03:36 +0100

    [w8] Login form

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,7 +3,19 @@
   <div class="page">
 
    <h1>{{ msg }}</h1>
+   <form class="login input-group col-md-offset-4 col-md-4" @submit.prevent="login">
 
+              <div class="input input-with-icon full-width ">
+
+              <input   class="input" required v-model="email" type="text" placeholder="email" />
+             <i class="input-icon fa fa-user"></i> </div>
+
+             <div class="input input-with-icon full-width ">
+              <input class="input " required v-model="password" type="password" placeholder="Password"/>
+              <i class="input-icon fa fa-key"></i>
+            </div>
+              <button class="button" type="submit">Login</button>
+            </form>
   </div>
 
 </template>
