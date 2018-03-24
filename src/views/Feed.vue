bbcf34f6fa941e453f02146b960ad16fecefa580
Maija <hello@maijagrudule.com>
2018-03-24 16:33:20 +0100

    [w10] Feed test with filler data

--- src/views/Feed.vue
+++ src/views/Feed.vue
@@ -3,19 +3,44 @@
   <div class="page">
 
    <h1>{{ msg }}</h1>
+   <div class="row">
+         <div v-for="(post, index) in posts" :key="post.id" class="col-md-4 ">
+        <h5> {{post.title}} </h5>
+        <hr>
+
+        <div> {{post.body}} </div>
+        <br>
+         </div>
+
+     </div>
 
   </div>
 
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'Feed',
   data () {
     return {
-      msg: 'Feed page'
+      msg: 'Feed page',
+      posts: [],
     }
-  }
+  },
+  mounted(){
+
+   axios.get("https://jsonplaceholder.typicode.com/posts",
+    )
+
+       .then((response)  =>  {
+         console.log(response)
+         this.posts = response.data;
+
+       }, (error)  =>  {
+         this.loading = false;
+       })
+     },
 }
 </script>
 
